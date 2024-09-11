6b8a67a59ce5516ae236a9bf0153ce355ebc11ff
vanomark <rudiger.kaspar@mail.ru>
2024-09-11 21:20:21 +0300

    Add files via upload

--- onegin.cpp
+++ onegin.cpp
@@ -4,12 +4,12 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-const int number_of_raws = 100;
+const int number_of_raws = 6000;
 const int length_of_raw = 80;
 
-void bubble_sort(char *index[]);
-int turn_lowercase(char *str);
-int string_compare(const char *str1, const char *str2);
+void    bubble_sort          (char* *index);
+int     turn_lowercase       (char *str);
+int     string_compare       (const char *str1, const char *str2);
 
 int main()
 {   
@@ -17,43 +17,26 @@ int main()
     char text[number_of_raws][length_of_raw] = {};
     char *index[number_of_raws] = {};
 
-
     FILE *onegin = fopen("start_file.txt", "r");
     assert(onegin);
 
     for (int i = 0; i < number_of_raws; i++) {
         fgets(string, length_of_raw, onegin);
-        printf("%s\n", string);
-        strcpy(text[i], string);
+        if (isalpha(string[0]))
+            strcpy(text[i], string);
     }   
     fclose(onegin);
 
     for (int i = 0; i < number_of_raws; i++)
             index[i] = text[0] + i * length_of_raw;
 
-    for (int i = 0; i < number_of_raws; i++) {
-        printf("%d: %s\n", (size_t) index[i], index[i]);
-
-        for (int j = 0; j < length_of_raw; j++) {
-            printf("%d: %c\n", (size_t) index[i] + j, index[i][j]);
-        }
-    }
-
-
-    printf("%d\n", strcmp("abcd", "abcs"));
-    bubble_sort(&index[0]);
+    bubble_sort(index);
 
     FILE *finish_file = fopen("finish_file.txt", "w");
-
     assert(finish_file);
 
     for (int i = 0; i < number_of_raws; i++) {
-        printf("%d: %s\n", (size_t) index[i], index[i]);
         fprintf(finish_file, "%s", index[i]);
-
-        for (int j = 0; j < length_of_raw; j++) {
-            printf("%d: %c\n", (size_t) index[i] + j, index[i][j]);
-        }
     }
 
     return 0;
@@ -68,12 +51,9 @@ void bubble_sort(char* *index)
         for (int i = 1; i < number_of_raws; i++) {
 
             if (string_compare(index[i-1], index[i]) > 0) {
-
                 str = index[i-1];
                 index[i-1] = index[i];
                 index[i] = str;
-
-
             }
         }
     }
@@ -104,7 +84,7 @@ int string_compare(const char *str1, const char *str2)
 
         turn_lowercase(&ch1);
         turn_lowercase(&ch2);
-
+    
         if (ch1 - ch2 > 0)
             return 1;
         if (ch1 - ch2 < 0)
@@ -113,4 +93,3 @@ int string_compare(const char *str1, const char *str2)
 
     return 0;
 }
-
